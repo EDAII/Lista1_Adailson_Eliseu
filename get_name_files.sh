468ea0d9cd494c1a2e3e0d80a24e98d30e05fd2f
Eliseu Egewarth <eliseuegewarth@gmail.com>
2019-03-29 20:09:17 -0300

    Removendo aquivos da estrutura do projeto e adicionando script para baixar arquivos da fonte
    Signed-off-by: Eliseu Egewarth <eliseuegewarth@gmail.com>

---
+++ get_name_files.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+wget -c https://raw.githubusercontent.com/dominictarr/random-name/master/middle-names.json
+wget -c https://raw.githubusercontent.com/dominictarr/random-name/master/names.json
+wget -c https://raw.githubusercontent.com/rossgoodwin/american-names/master/surnames.json
